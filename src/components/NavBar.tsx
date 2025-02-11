31e482dccce8a64f4e6f0c3dad9fbbbe5317379a
Manuel <manuel.barreiro@ing.austral.edu.ar>
2025-02-10 22:51:25 -0300

    link and product prose padding

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,6 +3,7 @@
 import CustomAvatar from "./custom-avatar"
 import { useSession } from "next-auth/react" // Add this
 import ToggleTheme from "./theme/theme-toggle"
+import Link from "next/link"
 
 export function Navbar() {
   const { data: session } = useSession() // Add this
@@ -11,7 +12,9 @@ export function Navbar() {
     <nav className="mx-auto max-w-5xl border-b border-gray-200">
       <div className="flex h-16 items-center px-4 sm:px-0">
         <div className="flex flex-1 items-center justify-between">
-          <div className="font-semibold">gestion-productos</div>
+          <Link href={"/"} className="font-semibold">
+            gestion-productos
+          </Link>
           <div className="flex items-center gap-4">
             <ToggleTheme />
             {session?.user && <CustomAvatar session={session} />}
